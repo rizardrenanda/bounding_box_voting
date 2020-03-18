9247229037af3fe202283a671e3f8e461612e97f
rizardrenanda <61375583+rizardrenanda@users.noreply.github.com>
2020-03-18 16:24:35 +0800

    Add files via upload

--- box_voting.m
+++ box_voting.m
@@ -1,5 +1,8 @@
 function [boxes_final] = box_voting(boxes,boxes_temp,thresh)
-
+% Output = boxes_final 
+% Input  = boxes [x1, y1, x2, y2] left top right bottom 
+%          boxes_temp : set of region proposals 
+%          thresh : IoU threshold 
 B_size = size(boxes,1);
 boxes_final = boxes;
 for i = 1 : B_size
@@ -11,21 +14,21 @@ for i = 1 : B_size
     y1_temp = boxes_temp(:,2);
     x2_temp = boxes_temp(:,3);
     y2_temp = boxes_temp(:,4);
+    
+    % Compute IoU
     area = (x2-x1+1) .* (y2-y1+1);
     area_temp = (x2_temp-x1_temp+1) .* (y2_temp-y1_temp+1);
     xx1 = max(x1, x1_temp);
     yy1 = max(y1, y1_temp);
     xx2 = min(x2, x2_temp);
     yy2 = min(y2, y2_temp);
-    
     w = max(0.0, xx2-xx1+1);
-    h = max(0.0, yy2-yy1+1);
-    
+    h = max(0.0, yy2-yy1+1);    
     inter = w.*h;
     o = inter ./ (area+ area_temp- inter);
-    % only use IoU > thresh for voting
-    temp1 = double(o >=thresh);
     
+    % only use proposals with IoU > thresh for voting
+    temp1 = double(o >= thresh);    
     nomx1 = sum(boxes_temp(:,1).*temp1.*boxes_temp(:,5))+x1*boxes(i,5);
     nomy1 = sum(boxes_temp(:,2).*temp1.*boxes_temp(:,5))+y1*boxes(i,5);
     nomx2 = sum(boxes_temp(:,3).*temp1.*boxes_temp(:,5))+x2*boxes(i,5);
